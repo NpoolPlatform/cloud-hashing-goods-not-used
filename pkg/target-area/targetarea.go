3d9fa99a2451729d49b204f4a41024fed0945900
Zhao KK <kikakkz@hotmail.com>
2021-10-30 05:54:49 +0000

    Query info before modify it

--- pkg/target-area/targetarea.go
+++ pkg/target-area/targetarea.go
@@ -2,14 +2,44 @@ package targetarea
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+
 	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/db"
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/db/ent/targetarea"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 )
 
 func Create(ctx context.Context, in *npool.CreateTargetAreaRequest) (*npool.CreateTargetAreaResponse, error) {
+	info1, err := db.Client().
+		TargetArea.
+		Query().
+		Where(
+			targetarea.And(
+				targetarea.Continent(in.GetInfo().GetContinent()),
+				targetarea.Country(in.GetInfo().GetCountry()),
+			),
+		).
+		Limit(1).
+		All(ctx)
+
+	logger.Sugar().Infof("%v: %v", info1, err)
+	if err == nil {
+		if 0 < len(info1) {
+			return &npool.CreateTargetAreaResponse{
+				Info: &npool.TargetAreaInfo{
+					ID:        info1[0].ID.String(),
+					Continent: info1[0].Continent,
+					Country:   info1[0].Country,
+				},
+			}, nil
+		}
+	} else if err != sql.ErrNoRows {
+		return nil, err
+	}
+
 	info, err := db.Client().
 		TargetArea.
 		Create().
@@ -19,9 +49,6 @@ func Create(ctx context.Context, in *npool.CreateTargetAreaRequest) (*npool.Crea
 	if err != nil {
 		return nil, err
 	}
-
-	logger.Sugar().Infof("%v", in.GetInfo())
-	logger.Sugar().Infof("%v", info)
 
 	return &npool.CreateTargetAreaResponse{
 		Info: &npool.TargetAreaInfo{
