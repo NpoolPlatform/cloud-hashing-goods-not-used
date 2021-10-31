e5be304e19bc1cb8dc2c1179701c9f6620d2554e
Zhao KK <kikakkz@hotmail.com>
2021-10-31 10:08:39 +0000

    Generate log to var log

--- cmd/cloud-hashing-goods/main.go
+++ cmd/cloud-hashing-goods/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
+	"runtime"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/app"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/service-name"
+
 	cli "github.com/urfave/cli/v2"
 )
 
-const serviceName = "Cloud Hashing Goods"
+const serviceName = servicename.ServiceName
 
 func main() {
 	commands := cli.Commands{
@@ -18,11 +23,20 @@ func main() {
 
 	description := fmt.Sprintf("my %v service cli\nFor help on any individual command run <%v COMMAND -h>\n",
 		serviceName, serviceName)
-	err := app.Init(serviceName, description, "", "", nil, commands)
+
+	_, mainPath, _, ok := runtime.Caller(0)
+	if !ok {
+		logger.Sugar().Errorf("cannot get path of main.go")
+		return
+	}
+	err := app.Init(serviceName, description, "", "", path.Dir(mainPath), nil, commands)
 	if err != nil {
 		logger.Sugar().Errorf("fail to create %v: %v", serviceName, err)
 		return
 	}
+
+	logger.Sugar().Infof("run main.go from: %v", mainPath)
+
 	err = app.Run(os.Args)
 	if err != nil {
 		logger.Sugar().Errorf("fail to run %v: %v", serviceName, err)
---
+++ pkg/target-area/targetarea_test.go
@@ -0,0 +1,63 @@
+package targetarea
+
+import (
+	"context"
+	"fmt"
+	"path"
+	"runtime"
+	"testing"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+	db "github.com/NpoolPlatform/cloud-hashing-goods/pkg/db"
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/service-name"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/app"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	_, myPath, _, ok := runtime.Caller(0)
+	if !ok {
+		return
+	}
+
+	configPath := fmt.Sprintf("%s/../../cmd/cloud-hashing-goods", path.Dir(myPath))
+
+	app.Init(servicename.ServiceName, "", "", "", configPath, nil, nil)
+	db.Init()
+}
+
+func TestCRUD(t *testing.T) {
+	resp, err := Create(context.Background(), &npool.CreateTargetAreaRequest{
+		Info: &npool.TargetAreaInfo{
+			Continent: "Asia",
+			Country:   "China",
+		},
+	})
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp) {
+		if assert.NotNil(t, resp.Info) {
+			assert.Equal(t, resp.Info.Continent, "Asia")
+			assert.Equal(t, resp.Info.Country, "China")
+		}
+	}
+
+	resp1, err := Update(context.Background(), &npool.UpdateTargetAreaRequest{
+		Info: resp.Info,
+	})
+	assert.Nil(t, err)
+	if assert.NotNil(t, resp1) {
+		if assert.NotNil(t, resp1.Info) {
+			assert.Equal(t, resp1.Info.ID, resp.Info.ID)
+			assert.Equal(t, resp1.Info.Continent, "Asia")
+			assert.Equal(t, resp1.Info.Country, "China")
+		}
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	resp, err := GetAll(context.Background(), &npool.GetTargetAreasRequest{})
+	assert.NotNil(t, resp)
+	assert.Nil(t, err)
+}
