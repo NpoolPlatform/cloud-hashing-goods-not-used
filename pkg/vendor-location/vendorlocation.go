71c1e72f168870bbbcae0a66034c898bf5aa154b
Zhao KK <kikakkz@hotmail.com>
2021-11-02 01:46:34 +0000

    Support create vendor location

--- pkg/vendor-location/vendorlocation.go
+++ pkg/vendor-location/vendorlocation.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
 
-	_ "github.com/NpoolPlatform/cloud-hashing-goods/pkg/db"                //nolint
-	_ "github.com/NpoolPlatform/cloud-hashing-goods/pkg/db/ent/targetarea" //nolint
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/db"                      //nolint
+	_ "github.com/NpoolPlatform/cloud-hashing-goods/pkg/db/ent/vendorlocation" //nolint
 
 	_ "github.com/google/uuid" //nolint
 
@@ -14,7 +14,27 @@ import (
 )
 
 func Create(ctx context.Context, in *npool.CreateVendorLocationRequest) (*npool.CreateVendorLocationResponse, error) {
-	return nil, xerrors.Errorf("NOT IMPLEMENTED")
+	info, err := db.Client().
+		VendorLocation.
+		Create().
+		SetCountry(in.GetInfo().GetCountry()).
+		SetProvince(in.GetInfo().GetProvince()).
+		SetCity(in.GetInfo().GetCity()).
+		SetAddress(in.GetInfo().GetAddress()).
+		Save(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &npool.CreateVendorLocationResponse{
+		Info: &npool.VendorLocationInfo{
+			ID:       info.ID.String(),
+			Country:  info.Country,
+			Province: info.Province,
+			City:     info.City,
+			Address:  info.Address,
+		},
+	}, nil
 }
 
 func Update(ctx context.Context, in *npool.UpdateVendorLocationRequest) (*npool.UpdateVendorLocationResponse, error) {
