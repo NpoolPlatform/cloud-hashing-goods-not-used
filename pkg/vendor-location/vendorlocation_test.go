e40f3204ab4d6068792e8cf216da5cb101caf019
Zhao KK <kikakkz@hotmail.com>
2021-11-02 01:41:32 +0000

    Support create test

--- pkg/vendor-location/vendorlocation_test.go
+++ pkg/vendor-location/vendorlocation_test.go
@@ -1,14 +1,47 @@
 package vendorlocation
 
-/*
 import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
 	"testing"
+	"time"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/test-init" //nolint
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestVendorLocationCRUD(t *testing.T) { //nolint
-	// country := "ChinaVendorLocationPackageTest"
-	// province := "ShanghaiVendorLocationPackageTest"
-	// city := "ShanghaiVendorLocationPackageTest"
-	// address := "YangpuVendorLocationPakcageTest"
+func init() {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
 }
-*/
+
+func TestVendorLocationCRUD(t *testing.T) { //nolint
+	nano := time.Now().UnixNano()
+	country := fmt.Sprintf("ChinaVendorLocationPackageTest-%v", nano)
+	province := fmt.Sprintf("ShanghaiVendorLocationPackageTest-%v", nano)
+	city := fmt.Sprintf("ShanghaiVendorLocationPackageTest-%v", nano)
+	address := fmt.Sprintf("YangpuVendorLocationPakcageTest-%v", nano)
+
+	resp, err := Create(context.Background(), &npool.CreateVendorLocationRequest{
+		Info: &npool.VendorLocationInfo{
+			Country:  country,
+			Province: province,
+			City:     city,
+			Address:  address,
+		},
+	})
+	if assert.Nil(t, err) {
+		assert.Equal(t, resp.Info.Country, country)
+		assert.Equal(t, resp.Info.Province, province)
+		assert.Equal(t, resp.Info.City, city)
+		assert.Equal(t, resp.Info.Address, address)
+	}
+}
