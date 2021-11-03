c16414ad3d0f2a38cbe5792e238bd1f91a3cc986
Zhao KK <kikakkz@hotmail.com>
2021-11-03 13:37:07 +0000

    Add good review api

--- api/goodreview.go
+++ api/goodreview.go
@@ -1,1 +1,43 @@
+// +build !codeanalysis
+
 package api
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/crud/good-review" //nolint
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) CreateGoodReview(ctx context.Context, in *npool.CreateGoodReviewRequest) (*npool.CreateGoodReviewResponse, error) {
+	resp, err := goodreview.Create(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("create good review error: %w", err)
+		return &npool.CreateGoodReviewResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
+
+func (s *Server) UpdateGoodReview(ctx context.Context, in *npool.UpdateGoodReviewRequest) (*npool.UpdateGoodReviewResponse, error) {
+	resp, err := goodreview.Update(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("update good review error: %w", err)
+		return &npool.UpdateGoodReviewResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
+
+func (s *Server) GetGoodReview(ctx context.Context, in *npool.GetGoodReviewRequest) (*npool.GetGoodReviewResponse, error) {
+	resp, err := goodreview.Get(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("delete good review error: %w", err)
+		return &npool.GetGoodReviewResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
--- api/goodreview_test.go
+++ api/goodreview_test.go
@@ -1,1 +1,88 @@
 package api
+
+import (
+	"encoding/json"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+)
+
+func assertGoodReview(t *testing.T, actual, expected *npool.GoodReviewInfo) {
+	assert.Equal(t, actual.Type, expected.Type)
+	assert.Equal(t, actual.ReviewerID, expected.ReviewerID)
+	assert.Equal(t, actual.ReviewedID, expected.ReviewedID)
+	assert.Equal(t, actual.State, expected.State)
+	assert.Equal(t, actual.Message, expected.Message)
+}
+
+func TestCreateGoodReview(t *testing.T) { //nolint
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+
+	goodReview := npool.GoodReviewInfo{
+		Type:       "good",
+		ReviewerID: uuid.New().String(),
+		ReviewedID: uuid.New().String(),
+		State:      "none",
+		Message:    "for review test",
+	}
+	firstCreateInfo := npool.CreateGoodReviewResponse{}
+
+	cli := resty.New()
+
+	resp, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.CreateGoodReviewRequest{
+			Info: &goodReview,
+		}).
+		Post("http://localhost:33759/v1/create/good/review")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp.StatusCode())
+		err := json.Unmarshal(resp.Body(), &firstCreateInfo)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, firstCreateInfo.Info.ID, uuid.New())
+			assertGoodReview(t, firstCreateInfo.Info, &goodReview)
+		}
+	}
+
+	goodReview.ID = firstCreateInfo.Info.ID
+
+	resp, err = cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.UpdateGoodReviewRequest{
+			Info: &goodReview,
+		}).
+		Post("http://localhost:33759/v1/update/good/review")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp.StatusCode())
+		info := npool.UpdateGoodReviewResponse{}
+		err := json.Unmarshal(resp.Body(), &info)
+		if assert.Nil(t, err) {
+			assert.Equal(t, info.Info.ID, firstCreateInfo.Info.ID)
+			assertGoodReview(t, info.Info, &goodReview)
+		}
+	}
+
+	resp, err = cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.GetGoodReviewRequest{
+			Info: &goodReview,
+		}).
+		Post("http://localhost:33759/v1/get/good/review")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp.StatusCode())
+		info := npool.GetGoodReviewResponse{}
+		err := json.Unmarshal(resp.Body(), &info)
+		if assert.Nil(t, err) {
+			assert.Equal(t, info.Info.ID, firstCreateInfo.Info.ID)
+			assertGoodReview(t, info.Info, &goodReview)
+		}
+	}
+}
