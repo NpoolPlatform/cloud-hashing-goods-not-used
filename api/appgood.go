9f01f319588d020be20d20d75c3e67cbd03e10cd
Zhao KK <kikakkz@hotmail.com>
2021-11-03 06:34:05 +0000

    Add app good apis

--- api/appgood.go
+++ api/appgood.go
@@ -1,15 +1,69 @@
+// +build !codeanalysis
+
 package api
 
 import (
 	"context"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
 	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/app-good" //nolint
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *Server) AuthorizeAppGood(ctx context.Context, in *npool.AuthorizeAppGoodRequest) (*npool.AuthorizeAppGoodResponse, error) {
-	return nil, nil
+	resp, err := appgood.Authorize(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("authorize app good error: %w", err)
+		return &npool.AuthorizeAppGoodResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
+
+func (s *Server) SetAppGoodPrice(ctx context.Context, in *npool.SetAppGoodPriceRequest) (*npool.SetAppGoodPriceResponse, error) {
+	resp, err := appgood.SetAppGoodPrice(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("authorize app good error: %w", err)
+		return &npool.SetAppGoodPriceResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
+
+func (s *Server) CheckAppGood(ctx context.Context, in *npool.CheckAppGoodRequest) (*npool.CheckAppGoodResponse, error) {
+	resp, err := appgood.Check(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("authorize app good error: %w", err)
+		return &npool.CheckAppGoodResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
+
+func (s *Server) OnsaleAppGood(ctx context.Context, in *npool.OnsaleAppGoodRequest) (*npool.OnsaleAppGoodResponse, error) {
+	resp, err := appgood.Onsale(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("authorize app good error: %w", err)
+		return &npool.OnsaleAppGoodResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
+
+func (s *Server) OffsaleAppGood(ctx context.Context, in *npool.OffsaleAppGoodRequest) (*npool.OffsaleAppGoodResponse, error) {
+	resp, err := appgood.Offsale(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("authorize app good error: %w", err)
+		return &npool.OffsaleAppGoodResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
 }
 
 func (s *Server) UnauthorizeAppGood(ctx context.Context, in *npool.UnauthorizeAppGoodRequest) (*npool.UnauthorizeAppGoodResponse, error) {
-	return nil, nil
+	resp, err := appgood.Unauthorize(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("authorize app good error: %w", err)
+		return &npool.UnauthorizeAppGoodResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
 }
--- api/appgood_test.go
+++ api/appgood_test.go
@@ -1,1 +1,150 @@
 package api
+
+import (
+	"encoding/json"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+)
+
+func assertAppGoodInfo(t *testing.T, actual, expected *npool.AppGoodInfo) {
+	assert.Equal(t, actual.AppID, expected.AppID)
+	assert.Equal(t, actual.GoodID, expected.GoodID)
+	assert.Equal(t, actual.Authorized, expected.Authorized)
+	assert.Equal(t, actual.Online, expected.Online)
+	assert.Equal(t, actual.InitAreaStrategy, expected.InitAreaStrategy)
+	assert.Equal(t, actual.Price, expected.Price)
+	assert.Equal(t, actual.GasPrice, expected.GasPrice)
+}
+
+func TestAppGoodCRUD(t *testing.T) { //nolint
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+
+	appGood := npool.AppGoodInfo{
+		AppID:  uuid.New().String(),
+		GoodID: uuid.New().String(),
+		// For result assert
+		Authorized:       true,
+		Online:           false,
+		InitAreaStrategy: "none",
+		Price:            0,
+		GasPrice:         0,
+	}
+	firstCreateInfo := npool.AuthorizeAppGoodRequest{}
+
+	cli := resty.New()
+
+	resp, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.AuthorizeAppGoodRequest{
+			Info: &appGood,
+		}).
+		Post("http://localhost:33759/v1/authorize/app/good")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp.StatusCode())
+		err := json.Unmarshal(resp.Body(), &firstCreateInfo)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, firstCreateInfo.Info.ID, uuid.UUID{})
+			assertAppGoodInfo(t, firstCreateInfo.Info, &appGood)
+		}
+	}
+
+	appGood.ID = firstCreateInfo.Info.ID
+
+	resp1, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.CheckAppGoodRequest{
+			Info: &appGood,
+		}).
+		Post("http://localhost:33759/v1/check/app/good")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp1.StatusCode())
+		info := npool.CheckAppGoodRequest{}
+		err := json.Unmarshal(resp.Body(), &info)
+		if assert.Nil(t, err) {
+			assert.Equal(t, info.Info.ID, firstCreateInfo.Info.ID)
+			assertAppGoodInfo(t, info.Info, &appGood)
+		}
+	}
+
+	appGood.Price = 0.13
+	appGood.GasPrice = 0.000013
+
+	resp3, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.SetAppGoodPriceRequest{
+			Info: &appGood,
+		}).
+		Post("http://localhost:33759/v1/set/app/good/price")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp3.StatusCode())
+		info := npool.SetAppGoodPriceRequest{}
+		err := json.Unmarshal(resp3.Body(), &info)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, info.Info.ID, uuid.UUID{})
+			assertAppGoodInfo(t, info.Info, &appGood)
+		}
+	}
+
+	appGood.Online = true
+
+	resp4, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.OnsaleAppGoodRequest{
+			Info: &appGood,
+		}).
+		Post("http://localhost:33759/v1/onsale/app/good")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp4.StatusCode())
+		info := npool.OnsaleAppGoodRequest{}
+		err := json.Unmarshal(resp4.Body(), &info)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, info.Info.ID, uuid.UUID{})
+			assertAppGoodInfo(t, info.Info, &appGood)
+		}
+	}
+
+	appGood.Online = false
+
+	resp5, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.OffsaleAppGoodRequest{
+			Info: &appGood,
+		}).
+		Post("http://localhost:33759/v1/offsale/app/good")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp5.StatusCode())
+		info := npool.OffsaleAppGoodRequest{}
+		err := json.Unmarshal(resp5.Body(), &info)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, info.Info.ID, uuid.UUID{})
+			assertAppGoodInfo(t, info.Info, &appGood)
+		}
+	}
+
+	appGood.Authorized = false
+
+	resp6, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.UnauthorizeAppGoodRequest{
+			Info: &appGood,
+		}).
+		Post("http://localhost:33759/v1/unauthorize/app/good")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp6.StatusCode())
+		info := npool.UnauthorizeAppGoodRequest{}
+		err := json.Unmarshal(resp6.Body(), &info)
+		if assert.Nil(t, err) {
+			assert.NotEqual(t, info.Info.ID, uuid.UUID{})
+			assertAppGoodInfo(t, info.Info, &appGood)
+		}
+	}
+}
