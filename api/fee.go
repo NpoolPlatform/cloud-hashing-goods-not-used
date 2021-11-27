b86d88c46ad97947a211b34fdf0443d06cbb978b
Zhao KK <kikakkz@hotmail.com>
2021-11-27 11:29:02 +0000

    Add fee and good fee apis

---
+++ api/fee.go
@@ -0,0 +1,34 @@
+// +build !codeanalysis
+
+package api
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/crud/fee" //nolint
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) CreateFee(ctx context.Context, in *npool.CreateFeeRequest) (*npool.CreateFeeResponse, error) {
+	resp, err := fee.Create(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("create fee error: %w", err)
+		return &npool.CreateFeeResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
+
+func (s *Server) GetFee(ctx context.Context, in *npool.GetFeeRequest) (*npool.GetFeeResponse, error) {
+	resp, err := fee.Get(ctx, in)
+	if err != nil {
+		logger.Sugar().Errorf("get fee error: %w", err)
+		return &npool.GetFeeResponse{}, status.Error(codes.Internal, "internal server error")
+	}
+	return resp, nil
+}
