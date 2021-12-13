5c688233655deb3b4a1cdcbd5f8fab5052fdd891
dengjie <dj@lifesdk.com>
2021-12-13 15:55:50 +0800

    skip test when github

--- pkg/middleware/recommend-goods/recommendgoods_test.go
+++ pkg/middleware/recommend-goods/recommendgoods_test.go
@@ -2,13 +2,24 @@ package recommendgoods
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+	testinit "github.com/NpoolPlatform/cloud-hashing-goods/pkg/test-init" //nolint
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRecommendGoodsCRUD(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
+
 	resp1, err := Get(context.Background(), &npool.GetRecommendGoodsRequest{
 		PageInfo: &npool.PageInfo{
 			PageIndex: 0,
