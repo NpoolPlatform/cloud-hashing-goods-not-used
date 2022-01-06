689c71d6148a8fb0d432848007536feb40d9f985
Zhao KK <kikakkz@hotmail.com>
2022-01-06 10:56:28 +0000

    Add create recommend api

--- pkg/crud/recommend/recommend.go
+++ pkg/crud/recommend/recommend.go
@@ -2,12 +2,76 @@ package recommend
 
 import (
 	"context"
+	"time"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/db"
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/db/ent"
 
 	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+
+	"github.com/google/uuid"
+
+	"golang.org/x/xerrors"
 )
 
+const (
+	dbTimeout = 5 * time.Second
+)
+
+func validateRecommend(info *npool.Recommend) error {
+	if _, err := uuid.Parse(info.GetAppID()); err != nil {
+		return xerrors.Errorf("invalid app id: %v", err)
+	}
+	if _, err := uuid.Parse(info.GetGoodID()); err != nil {
+		return xerrors.Errorf("invalid good id: %v", err)
+	}
+	if _, err := uuid.Parse(info.GetRecommenderID()); err != nil {
+		return xerrors.Errorf("invalid recommender id: %v", err)
+	}
+	if info.GetMessage() == "" {
+		return xerrors.Errorf("invalid recommend message")
+	}
+	return nil
+}
+
+func dbRowToRecommend(row *ent.Recommend) *npool.Recommend {
+	return &npool.Recommend{
+		ID:            row.ID.String(),
+		AppID:         row.AppID.String(),
+		GoodID:        row.GoodID.String(),
+		RecommenderID: row.RecommenderID.String(),
+		Message:       row.Message,
+	}
+}
+
 func Create(ctx context.Context, in *npool.CreateRecommendRequest) (*npool.CreateRecommendResponse, error) {
-	return nil, nil
+	if err := validateRecommend(in.GetInfo()); err != nil {
+		return nil, xerrors.Errorf("invalid parameter: %v", err)
+	}
+
+	cli, err := db.Client()
+	if err != nil {
+		return nil, xerrors.Errorf("fail get db client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, dbTimeout)
+	defer cancel()
+
+	info, err := cli.
+		Recommend.
+		Create().
+		SetAppID(uuid.MustParse(in.GetInfo().GetAppID())).
+		SetGoodID(uuid.MustParse(in.GetInfo().GetGoodID())).
+		SetRecommenderID(uuid.MustParse(in.GetInfo().GetRecommenderID())).
+		SetMessage(in.GetInfo().GetMessage()).
+		Save(ctx)
+	if err != nil {
+		return nil, xerrors.Errorf("fail careate recommend: %v", err)
+	}
+
+	return &npool.CreateRecommendResponse{
+		Info: dbRowToRecommend(info),
+	}, nil
 }
 
 func Update(ctx context.Context, in *npool.UpdateRecommendRequest) (*npool.UpdateRecommendResponse, error) {
---
+++ pkg/crud/recommend/recommend_test.go
@@ -0,0 +1,49 @@
+package recommend
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/NpoolPlatform/cloud-hashing-goods/message/npool"
+	"github.com/NpoolPlatform/cloud-hashing-goods/pkg/test-init" //nolint
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/google/uuid"
+)
+
+func init() {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
+}
+
+func assertRecommend(t *testing.T, actual, expected *npool.Recommend) {
+	assert.Equal(t, actual.AppID, expected.AppID)
+	assert.Equal(t, actual.GoodID, expected.GoodID)
+	assert.Equal(t, actual.RecommenderID, expected.RecommenderID)
+	assert.Equal(t, actual.Message, expected.Message)
+}
+
+func TestCRUD(t *testing.T) {
+	recommend := npool.Recommend{
+		AppID:         uuid.New().String(),
+		GoodID:        uuid.New().String(),
+		RecommenderID: uuid.New().String(),
+		Message:       "recommend test good",
+	}
+
+	resp, err := Create(context.Background(), &npool.CreateRecommendRequest{
+		Info: &recommend,
+	})
+	if assert.Nil(t, err) {
+		assert.NotEqual(t, resp.Info.ID, uuid.UUID{}.String())
+		assertRecommend(t, resp.Info, &recommend)
+	}
+}
