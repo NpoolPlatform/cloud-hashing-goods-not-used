1617eaa6960c789d3f14ee99b803466bb1534b3f
Zhao KK <kikakkz@hotmail.com>
2022-02-12 10:47:11 +0000

    Return the error

--- api/vendorlocation.go
+++ api/vendorlocation.go
@@ -18,8 +18,8 @@ import (
 func (s *Server) CreateVendorLocation(ctx context.Context, in *npool.CreateVendorLocationRequest) (*npool.CreateVendorLocationResponse, error) {
 	resp, err := vendorlocation.Create(ctx, in)
 	if err != nil {
-		logger.Sugar().Errorf("create vendor location error: %w", err)
-		return &npool.CreateVendorLocationResponse{}, status.Error(codes.Internal, "internal server error")
+		logger.Sugar().Errorf("create vendor location error: %v", err)
+		return &npool.CreateVendorLocationResponse{}, status.Error(codes.Internal, err.Error())
 	}
 	return resp, nil
 }
@@ -27,8 +27,8 @@ func (s *Server) CreateVendorLocation(ctx context.Context, in *npool.CreateVendo
 func (s *Server) UpdateVendorLocation(ctx context.Context, in *npool.UpdateVendorLocationRequest) (*npool.UpdateVendorLocationResponse, error) {
 	resp, err := vendorlocation.Update(ctx, in)
 	if err != nil {
-		logger.Sugar().Errorf("update vendor location error: %w", err)
-		return &npool.UpdateVendorLocationResponse{}, status.Error(codes.Internal, "internal server error")
+		logger.Sugar().Errorf("update vendor location error: %v", err)
+		return &npool.UpdateVendorLocationResponse{}, status.Error(codes.Internal, err.Error())
 	}
 	return resp, nil
 }
@@ -36,8 +36,8 @@ func (s *Server) UpdateVendorLocation(ctx context.Context, in *npool.UpdateVendo
 func (s *Server) GetVendorLocation(ctx context.Context, in *npool.GetVendorLocationRequest) (*npool.GetVendorLocationResponse, error) {
 	resp, err := vendorlocation.Get(ctx, in)
 	if err != nil {
-		logger.Sugar().Errorf("delete vendor location error: %w", err)
-		return &npool.GetVendorLocationResponse{}, status.Error(codes.Internal, "internal server error")
+		logger.Sugar().Errorf("delete vendor location error: %v", err)
+		return &npool.GetVendorLocationResponse{}, status.Error(codes.Internal, err.Error())
 	}
 	return resp, nil
 }
@@ -45,8 +45,8 @@ func (s *Server) GetVendorLocation(ctx context.Context, in *npool.GetVendorLocat
 func (s *Server) DeleteVendorLocation(ctx context.Context, in *npool.DeleteVendorLocationRequest) (*npool.DeleteVendorLocationResponse, error) {
 	resp, err := vendorlocation.Delete(ctx, in)
 	if err != nil {
-		logger.Sugar().Errorf("delete vendor location error: %w", err)
-		return &npool.DeleteVendorLocationResponse{}, status.Error(codes.Internal, "internal server error")
+		logger.Sugar().Errorf("delete vendor location error: %v", err)
+		return &npool.DeleteVendorLocationResponse{}, status.Error(codes.Internal, err.Error())
 	}
 	return resp, nil
 }
@@ -54,8 +54,8 @@ func (s *Server) DeleteVendorLocation(ctx context.Context, in *npool.DeleteVendo
 func (s *Server) GetVendorLocations(ctx context.Context, in *npool.GetVendorLocationsRequest) (*npool.GetVendorLocationsResponse, error) {
 	resp, err := vendorlocation.GetAll(ctx, in)
 	if err != nil {
-		logger.Sugar().Errorf("get vendor locations error: %w", err)
-		return &npool.GetVendorLocationsResponse{}, status.Error(codes.Internal, "internal server error")
+		logger.Sugar().Errorf("get vendor locations error: %v", err)
+		return &npool.GetVendorLocationsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 	return resp, nil
 }
