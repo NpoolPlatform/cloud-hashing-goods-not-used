da39716488cf355ea28064f4c109f8e543a12cd0
Zhao KK <kikakkz@hotmailcom>
2022-06-03 05:26:28 +0000

    Return all visible good

--- pkg/middleware/good-detail/gooddetail.go
+++ pkg/middleware/good-detail/gooddetail.go
@@ -157,7 +157,7 @@ func GetByApp(ctx context.Context, in *npool.GetGoodsDetailByAppRequest) (*npool
 		price := info.Price
 
 		for _, appGood := range appGoods.Infos {
-			if info.ID == appGood.GoodID && appGood.Visible {
+			if info.ID == appGood.GoodID {
 				price = appGood.Price
 				allowed = true
 				break
